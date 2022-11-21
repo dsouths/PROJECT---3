12d702aac5d69b2223c405f74941197c138d2462
dsouths <dsouths@hotmail.com>
2022-11-21 20:46:21 +0000

    adding if & else statements to account for guessing wrong letter & word & what happens if letter is guessed correctly

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ def play_game(word):
     #keeps track of the game state
     guessed = False
     dashed_word = "_" * len(word)
+    attempts = 6
     print("\n*****Welcome to the World Cup 22 Edition of Hangman!*****\n")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Try & guess the name of the team playing in the tournament")
@@ -23,4 +24,14 @@ def play_game(word):
         guess = input("Kick-off by guessing a letter or word: ").upper() 
         if len(guess) == 1 and isalpha():
             if guess in guessed_letters:
-                print("You already guessed the letter, better change tactics!!", guess) 
+                print("You already guessed this LETTER, better change tactics!!", guess)
+            elif guess in guessed_words:
+                print("You already guessed this WORD, better change tactics!!", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.") 
+                attempts -= 1
+                guessed_letters.append(guess)
+            else:
+                print("GGGGGOAL,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(dashed_word) 
