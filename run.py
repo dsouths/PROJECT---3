ec4b8ea5798d1eea3f008e714b806c328a1050ca
dsouths <dsouths@hotmail.com>
2022-11-22 09:52:00 +0000

    create if statement to print result in console if player wins or loses

--- run.py
+++ run.py
@@ -14,7 +14,9 @@ def play_game(word):
     guessed_words = []
     #keeps track of the game state
     guessed = False
+    #replaces word with same amount of dashes as letters in word
     dashed_word = "_" * len(word)
+    #max attempts player has to guess word
     attempts = 6
     print("\n*****Welcome to the World Cup 22 Edition of Hangman!*****\n")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -22,16 +24,36 @@ def play_game(word):
     
     while not guessed and attempts > 0:
         guess = input("Kick-off by guessing a letter or word: ").upper() 
-        if len(guess) == 1 and isalpha():
-            if guess in guessed_letters:
-                print("You already guessed this LETTER, better change tactics!!", guess)
-            elif guess in guessed_words:
-                print("You already guessed this WORD, better change tactics!!", guess)
-            elif guess not in word:
-                print(guess, "is not in the word.") 
-                attempts -= 1
-                guessed_letters.append(guess)
-            else:
-                print("GGGGGOAL,", guess, "is in the word!")
-                guessed_letters.append(guess)
-                word_as_list = list(dashed_word) 
+        if len(guess) != 1:
+            print('Please enter a single letter. YELLOW CARD!!')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        elif guess in guessed_letters:
+            print("You already guessed this LETTER, better change tactics!!", guess)
+        elif guess in guessed_words:
+            print("What a miss!! You already guessed this WORD!!", guess)
+        elif guess not in word:
+            print(guess, "is not in the word.") 
+            attempts -= 1
+            guessed_letters.append(guess)
+        else:
+            print("GGGGGOAL,", guess, "is in the word!")
+            guessed_letters.append(guess)
+            word_as_list = list(dashed_word)
+            indices = [i for i, letter in enumerate(word) if letter == guess]             
+            for index in indices: 
+                word_as_list[index] = guess 
+            dashed_word = "".join(word_as_list) 
+            if "_" not in dashed_word: 
+                guessed = True     
+        print(display_hangman(attempts)) 
+        print(dashed_word) 
+        print("\n")
+    
+    if guessed: 
+        print("RESULT!!! You have won the match & guessed the word correctly! Olé Olé Olé...")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+    
+    def display_hangman(attempts):
+        
