7489872a3cb9afd8c359c74319ea3e453946ece1
dsouths <dsouths@hotmail.com>
2022-11-23 20:34:11 +0000

    fixing pep8 issues spacing & formating

--- run.py
+++ run.py
@@ -1,112 +1,41 @@
 import random
+from hangman import display_hangman
 
 word_list = ["Qatar", "Ecuador", "Senegal", "Netherlands", "England", "Iran", "USA", "Wales", "Argentina", "SaudiArabia", "Mexico", "Poland", "France", "Australia", "Denmark", "Tunisia", "Spain", "CostaRica", "Germany", "Japan", "Belgium", "Canada", "Morocco", "Croatia", "Brazil", "Serbia", "Switzerland", "Cameroon", "Portugal", "Ghana", "Uruguay", "SouthKorea"]
 
-def display_hangman(attempts):
-        stages = [  # final state:
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |     \|/ 
-                   |      | 
-                   |     / \ 
-                   - 
-                """, 
-                # 5
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |     \|/ 
-                   |      | 
-                   |     /  
-                   - 
-                """, 
-                # 4
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |     \|/ 
-                   |      | 
-                   |       
-                   - 
-                """, 
-                # 3
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |     \| 
-                   |      | 
-                   |      
-                   - 
-                """, 
-                # 2
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |      | 
-                   |      | 
-                   |      
-                   - 
-                """, 
-                # 1 
-                """ 
-                   -------- 
-                   |      | 
-                   |      O 
-                   |     
-                   |       
-                   |      
-                   - 
-                """, 
-                # initial state 
-                """ 
-                   -------- 
-                   |      | 
-                   |       
-                   |     
-                   |       
-                   |      
-                   - 
-                """ 
-    ] 
-        return stages[attempts] 
 
-#this function returns a random word from the word list & returns upper case to stop upper/lower case errors occuring later
+# this function returns a random word from the word list & returns upper case to stop upper/lower case errors occuring later
 def pick_word():
     word = random.choice(word_list)
     return word.upper()
-    
-#function to play game with chosen random word
+
+
+# function to play game with chosen random word
 def play_game(word):
-    #replaces word with same amount of dashes as letters in word
+    # replaces word with same amount of dashes as letters in word
     dashed_word = "~" * len(word)
-    #keeps track of the game state
+    # keeps track of the game state
     guessed = False
-    #keeps track of the guessed letters & words
+    # keeps track of the guessed letters & words
     guessed_letters = []
     guessed_words = []
-    #max attempts player has to guess word
+    # max attempts player has to guess word
     attempts = 6
-    print("\n*Welcome to the World Cup 22 Edition of Hangman!*\n")
+    print("\n**Welcome to the World Cup 22 Edition of Hangman!*\n")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     print("Guess the name of the team playing in the World Cup\n")
     print(display_hangman(attempts))
     print(dashed_word)
     while not guessed and attempts > 0:
-        guess = input("Pick a letter or word: ").upper() 
+        guess = input("Pick a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
                 print('YELLOW CARD!! Please enter a LETTER.')
             elif guess in guessed_letters:
-                print("You already guessed this LETTER, better change tactics!!", guess)
+                print("You guessed this LETTER, better change tactics!", guess)
                 print(guessed_letters)
             elif guess not in word:
-                print(guess, "is NOT in the word.") 
+                print(guess, "is NOT in the word.")
                 print(guessed_words)
                 attempts -= 1
                 guessed_letters.append(guess)
@@ -114,39 +43,39 @@ def play_game(word):
                 print("GGGGGOAL,", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(dashed_word)
-                indices = [i for i, letter in enumerate(word) if letter == guess]             
-                for index in indices: 
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
                     word_as_list[index] = guess 
-                dashed_word = "".join(word_as_list)     
-                if "~" not in dashed_word: 
+                dashed_word = "".join(word_as_list)  
+                if "~" not in dashed_word:
                     guessed = True   
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("What a miss!! You already guessed this WORD!!", guess)
             elif guess != word:
-                print("WHAT A MISS", guess, "is NOT the word!") 
-                attempts -= 1 
+                print("WHAT A MISS", guess, "is NOT the word!")
+                attempts -= 1
                 guessed_words.append(guess)
-            else: 
-                guessed = True 
+            else:
+                guessed = True
                 dashed_word = word
         else:
             print("NOT a valid guess!") 
         print(dashed_word) 
-        print(display_hangman(attempts))
-        
-    
+        print(display_hangman(attempts))       
     if guessed: 
         print("RESULT!!! You have won the match & guessed the word correctly! Olé Olé Olé...")
     else:
-        print("Sorry, you ran out of tries & lost the match! The word was " + word + ". Better luck next time!")
-    
-def main(): 
-    word = pick_word() 
-    play_game(word) 
-    while input("Play Again? (Y/N) ").upper() == "Y": 
-        word = pick_word() 
-        play_game(word) 
+        print(f"Sorry, you ran out of tries & lost the match! The word was {word}. Better luck next time!")
 
-if __name__ == "__main__": 
+
+def main():
+    word = pick_word()
+    play_game(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = pick_word()
+        play_game(word)
+
+
+if __name__ == "__main__":
     main()
